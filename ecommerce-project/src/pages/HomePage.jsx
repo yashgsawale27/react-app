6cb572ffb3dd15a6c8913db9df8396904cfb6ace
User Name <user@gmail.com>
2025-10-28 13:56:45 +0530

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,14 @@ import { products } from '../../starting -code/data/products.js'
 import './HomePage.css';
 
 export function HomePage() {
+    fetch('http://localhost:3000/api/products')
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            console.log(data);
+        });
+
+
     return (
         <>
             <title>Ecommerce Project</title>
