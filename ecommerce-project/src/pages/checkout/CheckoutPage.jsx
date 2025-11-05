4cebd24bf2087ace68c3dfe8f0b953c033148403
User Name <user@gmail.com>
2025-11-05 17:07:00 +0530

    Convert home and checkout to async await

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -10,15 +10,16 @@ export function CheckoutPage({ cart }) {
     const [paymentSummary, setPaymentSummary] = useState(null);
 
     useEffect(() => {
-        axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
-            .then((response) => {
-                setDeliveryOptions(response.data);
-            });
+        const fetchCheckout = async () => {
+            let response = await axios.get(
+                '/api/delivery-options?expand=estimatedDeliveryTime');
+            setDeliveryOptions(response.data);
 
-        axios.get('/api/payment-summary')
-            .then((response) => {
-                setPaymentSummary(response.data);
-            })
+            response = await axios.get('/api/payment-summary')
+            setPaymentSummary(response.data);
+        };
+
+        fetchCheckout();
     }, []);
 
     return (
--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -8,10 +8,12 @@ export function HomePage({ cart }) {
     const [products, setproducts] = useState([]);
 
     useEffect(() => {
-        axios.get('/api/products')
-            .then((response) => {
-                setproducts(response.data);
-            });
+        const getHomeData = async () => {
+            const response = await axios.get('/api/products');
+            setproducts(response.data);
+        };
+
+        getHomeData();
     }, []);
 
     return (
