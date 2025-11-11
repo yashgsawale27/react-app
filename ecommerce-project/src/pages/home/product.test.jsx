aa2db3818cfd6abebd7edd23cc4f023739899b9e
User Name <user@gmail.com>
2025-11-11 16:22:34 +0530

    Use beforEach to remove duplication

--- ecommerce-project/src/pages/home/product.test.jsx
+++ ecommerce-project/src/pages/home/product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Product } from './Product';
 import axios from 'axios';
@@ -7,8 +7,11 @@ import userEvent from '@testing-library/user-event';
 vi.mock('axios');
 
 describe('Product component', () => {
-    it('displays the product details correctly', () => {
-        const product = {
+    let product;
+    let loadCart;
+
+    beforeEach(() => {
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -20,8 +23,10 @@ describe('Product component', () => {
             keywords: ["socks", "sports", "apparel"]
         };
 
-        const loadCart = vi.fn();
+        loadCart = vi.fn();
+    });
 
+    it('displays the product details correctly', () => {
         render(<Product product={product} loadCart={loadCart} />);
 
         expect(
@@ -47,20 +52,6 @@ describe('Product component', () => {
     });
 
     it('adds a product to the cart', async () => {
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"]
-        };
-
-        const loadCart = vi.fn();
-
         render(<Product product={product} loadCart={loadCart} />);
 
         const user = userEvent.setup();
